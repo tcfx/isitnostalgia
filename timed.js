1c526e566a89d36824009e32c54eda565201e3f4
tcfx <leeanne1212@live.ca>
2022-06-17 23:04:11 -0400

    Initial commit

---
+++ timed.js
@@ -0,0 +1,10 @@
+$(window).on("load", start);
+
+ScrollReveal().reveal('.body');
+
+function start(){
+ 
+    ScrollReveal().reveal('.about',{delay: 900});
+    ScrollReveal().reveal('.posts',{delay: 900});
+    
+}
